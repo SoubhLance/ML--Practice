76f07987596952512c6b071c7948d377a43900fc
Soubhlance <soubhiksadhu1981@gmail.com>
2026-06-12 01:38:49 +0530

    pydantic_data_validation

--- Python/Fast-API/Pydantic/main1.py
+++ Python/Fast-API/Pydantic/main1.py
@@ -9,11 +9,18 @@
 
 ## To Fix that we are using the pydantic 
 
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr,AnyUrl,Field
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
-    name:str
-    age:int
+    name:str = Field(max_length=50)
+    age:int = Field(gt=0,lt=120)
+    email:EmailStr
+    linedin:AnyUrl
+    weight:float = Field(gt=0)
+    married:bool
+    allergies:Optional[List[str]]  = Field(max_length=5)
+    contact_details: Dict[str,str]
 
 
 def insert_patient_data(patient : Patient):
@@ -21,12 +28,25 @@ def insert_patient_data(patient : Patient):
     print(patient.name)
     print(patient.age)
 
+def update_patient_data(patient: Patient):
+
+    print(patient.name)
+    print(patient.age)
+    print("Updated Records")
+
 patient_info = {'name':'Soubhik','age':21}
 patient_info1 = {'name':'Soubhik','age':'tyhirty'} ## Checking the Wrong output 
+patient_info2 = {'name':'Soubhik','age':36}
+patient_info3 = {'name':'Soubhik','age':36, 'weight':75.2,'married': True,'allergies':['pollen','dust'], 'contact_details':{'email':'abc@gmail.com','phone':'9852458769'}}
 
 patient1 = Patient(**patient_info)
-patient2 = Patient(**patient_info1) ## wrong
+# patient2 = Patient(**patient_info1) ## wrong
+patient3 = Patient(**patient_info2)
+patient4 = Patient(**patient_info3)
 
 
 insert_patient_data(patient1)
-insert_patient_data(patient2)  ## here inserting and then validation error 
+print("patient2 data is not valid")
+# insert_patient_data(patient2)  ## here inserting and then validation error 
+update_patient_data(patient3)
+insert_patient_data(patient4)
